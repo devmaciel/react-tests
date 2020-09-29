e83ddef587eb98794d6a5396d8b9ec38658a5032
João Pedro Maciel <joomaciiel2@outlook.com>
2020-09-28 21:38:49 -0300

    useEffect

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,30 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
 
-  const [repositories, setRepositories] = useState([
-    { id: 1, name: 'repo-1'},
-    { id: 2, name: 'repo-2'},
-    { id: 3, name: 'repo-3'},
-  ]);
+  const [repositories, setRepositories] = useState([]);
 
-  function handleAddRepository(){
-    setRepositories([
-      ...repositories,
-      { id: Math.random(), name: "Novo repo" }
-    ])
+  
+  //sempre queo state mudar que esta dentro do array parametro do use effect,
+  //caso tenha algo no array, vai executar novamente, se não tiver, só vai chamar 1.
+  useEffect(async () => {
+    const response = await fetch('https://api.github.com/users/jorgjr/repos');
+    const data = await response.json();
+
+    setRepositories(data);
+  }, []);
+
+
+
+  //caso mude repositories, muda o numero total de favoritos, contagem todos repo com favorito
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+
+    document.title = `Você tem ${filtered.length} favoritos`;
+  }, [repositories]);
+
+
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? {...repo, favorite: !repo.favorite} : repo
+    });
+    
+    setRepositories(newRepositories);
   }
 
   return (
     <div className="App">
       <ul>
         {repositories.map(repo => (
-          <li key={repo.id}>{repo.name}</li>
+          <li key={repo.id}>
+            {repo.name}
+            {repo.favorite && <span>(Favorito)</span>}
+            <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+          </li>
         ))}
       </ul>
-      <button onClick={handleAddRepository}>
-        Adicionar repositório
-      </button>
     </div>
   );
 }
