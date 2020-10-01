af86faa40eedfe8c9d5cd5864b5e2c3846a7a047
João Pedro Maciel <joomaciiel2@outlook.com>
2020-10-01 19:42:26 -0300

    useDispatch

--- reduxhooks/src/components/CourseList/index.js
+++ reduxhooks/src/components/CourseList/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 export default function CourseList(){
 
     const courses = useSelector(state => state.data);
+    const dispatch = useDispatch();
+
+    // function addCourseAction(title){
+    //     return { type:'ADD_COURSE', title }
+    // }
+
+    function addCourse(){
+        //use dispatch
+        dispatch({ type: 'ADD_COURSE', title: 'GraphQL' })
+
+        //addcourseaction
+        // dispatch(addCourseAction('GraphQL))
+    }
 
     return (
-        <ul>
-            {
-                courses.map(course => <li key={course}>{course}</li>)
-            }
-        </ul>
+        <>
+            <ul>
+                {
+                    courses.map(course => <li key={course}>{course}</li>)
+                }
+            </ul>
+            <button type="button" onClick={addCourse}>Adicionar Curso</button>
+        </>
     );
 }
